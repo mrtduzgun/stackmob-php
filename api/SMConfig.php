26928efaa63543763b918f92d50cd35814120480
Murat Düzgün <mrtduzgun@gmail.com>
2016-11-27 19:41:44 +0200

    Update SMConfig.php

--- api/SMConfig.php
+++ api/SMConfig.php
@@ -5,9 +5,9 @@ class SMConfig {
 
     const apiUrl = 'https://api.stackmob.com/';
     
-    const consumerKey = "646fe24f-4fe2-45c1-a391-6561e6916538";
+    const consumerKey = "";
     
-    const consumerSecret = "ee5b7ac9-b0a1-4e1f-a0c1-018989597097";
+    const consumerSecret = "";
     
     /**
      *  version : Development => 0, Production => 1
